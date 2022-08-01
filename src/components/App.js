76de855dcc7d5a70ac64fa5f7a77934ca23879da
Nicholas <nicholasjoven@gmail.com>
2022-08-01 14:20:43 -0400

    fixed appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-
-=======
 import { useState, useEffect } from 'react'
->>>>>>> 6cb4bb07f996a527f1adf9a786bedbc2c3e6a5a8
 import Header from './Header'
 import Content from './Content'
 
 
 const App = () => {
+
   return (
     <div className="App">
       <Header />
@@ -17,3 +14,4 @@ const App = () => {
 }
 
 export default App;
+
