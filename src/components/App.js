2c755f9168e16e7109902f12c6e7f90cc929f32f
Agnes <agsawicka@gmail.com>
2022-08-01 12:06:54 -0400

    added user data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
+import {useEffect, useState} from 'react'
 import Header from './Header'
 import Content from './Content'
 
+
 const App = () => {
+  const[users, setUsers] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:8000/users')
+    .then(req => req.json())
+    .then(res => setUsers(res))
+  }, [])
+
+
   return (
     <div className="App">
       <Header />
       <Content />
+      {users.map(user => {return <div><p>{user.user_name}</p> <img src={user.image} alt="img" /> </div>
+    })}
 
     </div>
   );
