36ea747cf9165c8fb499ab806b6eeda4133fb771
Agnes <agsawicka@gmail.com>
2022-08-02 10:42:37 -0400

    date and time

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,7 +1,18 @@
-const UpcomingEvents = () => {
+const UpcomingEvents = ({event}) => {
+    console.log(event.date)
+    let date = new Date(event.date)
+    let day = date.getDate()
+    let dayName = date.toLocaleDateString('en-US', {weekday: 'short'})
+    let month = date.getMonth() + 1
+    let hour = date.toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit', })
+    let year = date.getFullYear()
+    
     return (
-        <div className='Upcoming-Events'>
-
+        <div className='Upcoming-Event'>
+            <p>{event.name}</p>
+            <p>{`${dayName},${day} ${month},${year}`}</p>
         </div>
     )
 }
--- src/components/UserScreen.js
+++ src/components/UserScreen.js
@@ -1,11 +1,20 @@
 import UserEvents from './UserEvents'
 import UpcomingEvents from './UpcomingEvents'
+import {useState, useEffect} from 'react'
 
 const UserScreen = () => {
+    const[events, setEvents]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:8000/events')
+        .then(req => req.json())
+        .then(res => setEvents(res))
+    }, [])
+
+
     return (
         <div className='User-Screen'>
             <UserEvents />
-            <UpcomingEvents />
+            <div className="Upcoming-Events">{events.map(event => <UpcomingEvents key={event.id} event={event}/>)}</div>
         </div>
     )
 }
