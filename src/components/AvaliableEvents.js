a12ebf1cfbaecdbef62815c698bbca37702ff40f
Agnes <agsawicka@gmail.com>
2022-08-02 17:32:22 -0400

    search events

--- src/components/AvaliableEvents.js
+++ src/components/AvaliableEvents.js
@@ -1,12 +1,18 @@
-import Event from './Event'
+import {useState} from 'react'
 import AvaliableEvent from './AvaliableEvent'
+import Calendar from 'react-calendar'
 
 const AvaliableEvents = ({events}) => {
+    // to do:
     // change events to display with sign which are attended by user (user id and check event id)
+    const[date, setDate]=useState(new Date())
+    
+
+
     return (
         <div>
         <div className="filters"> 
-            <button className="filter-button"> search by date </button>
+            <button className="filter-button"  > search by date </button>
             <button className="filter-button"> search by type </button>
             <button className="filter-button"> search by category </button>
         </div>  
