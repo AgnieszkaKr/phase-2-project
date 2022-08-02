a3d3ce370d22238cb97899ee0e3f9d3dd06dc460
Agnes <agsawicka@gmail.com>
2022-08-02 11:23:17 -0400

    onClick show details

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -1,18 +1,35 @@
+import {useState} from 'react'
+
 const UpcomingEvents = ({event}) => {
-    console.log(event.date)
-    let date = new Date(event.date)
-    let day = date.getDate()
-    let dayName = date.toLocaleDateString('en-US', {weekday: 'short'})
-    let month = date.getMonth() + 1
-    let hour = date.toLocaleTimeString('en-US', {
+    const[displayDetails, setDisplayDitails]=useState(false)
+    
+    const date = new Date(event.date)
+    const day = date.getDate()
+    const dayName = date.toLocaleDateString('en-US', {weekday: 'short'})
+    const month = date.toLocaleString('default', { month: 'short' })
+    const hour = date.toLocaleTimeString('en-US', {
             hour: '2-digit',
             minute: '2-digit', })
-    let year = date.getFullYear()
+    const year = date.getFullYear()
+
+    const handeleDisplayEvent =()=>{
+        setDisplayDitails(!displayDetails)
+    }
     
     return (
-        <div className='Upcoming-Event'>
+        <div className='Upcoming-Event' onClick={handeleDisplayEvent} >
+            
+            {displayDetails ? (
+            <p>
             <p>{event.name}</p>
-            <p>{`${dayName},${day} ${month},${year} ${hour}`}</p>
+            <img className="Upcoming-Event-Image" src={event.image} alt="" />
+            <p>{`${dayName}, ${day} ${month}, ${year} ${hour} EST`}</p>
+            <p>{event.description}</p>
+            </p>
+             ):(
+            <p>
+            <p>{event.name}</p>
+            <p>{`${dayName}, ${day} ${month}, ${year} ${hour} EST`}</p></p> )}
         </div>
     )
 }
