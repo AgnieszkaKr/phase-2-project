42831953931fe55bfcf8fb927f3373393a31d965
Nicholas <nicholasjoven@gmail.com>
2022-08-02 14:14:46 -0400

    updating user event filter

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,19 @@ import UserScreen from './UserScreen'
 const App = () => {
   const [users, setUsers] = useState([])
   const [currentUser, setCurrentUser] = useState({})
-<<<<<<< HEAD
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userEvents, setUserEvents] = useState([])
+  const [events, setEvents] = useState([])
+  
+  useEffect(() => {
+    const getEvents = () => {
+      fetch('http://localhost:8000/events')
+      .then(req => req.json())
+      .then(res => setEvents(res))
+    }
+    getEvents()
+  }, [])
+
 
   useEffect(() => {
     const getUsers = () => {
@@ -23,14 +34,34 @@ const App = () => {
     getUsers()
   }, [])
 
+  useEffect(() => {
+    const associateUserEvents = () => {
+      const _userEvents = events.filter((event) => {
+        return (
+          currentUser['joined_events'].includes(event.id)
+        )
+      })
+      setUserEvents(_userEvents)
+    }
+    associateUserEvents()
+  }, [currentUser])
+  
+
   const handleLoginSuccess = (user) => {
-    setCurrentUser(user)
-    // console.log('Hello, ', user.user_name)
+    handleCurrentUser(user)
     setIsLoggedIn(true)
   }
 
+  const handleCurrentUser = (user) => {
+    setCurrentUser(user)
+  }
+
+  
   return (
     <div className="App">
+      <button onClick={() => {
+        console.log(userEvents)
+      }}>Click</button>
       <Header 
         users={users} 
         currentUser={currentUser}
@@ -46,7 +77,10 @@ const App = () => {
           </div>
         : 
           <div className='show-after-login'>
-            <UserScreen />
+            <UserScreen
+              user={currentUser} 
+              joinedEvents={userEvents}
+            />
           </div>
       }
     </div>
--- src/components/UserScreen.js
+++ src/components/UserScreen.js
@@ -1,10 +1,15 @@
 import UserEvents from './UserEvents'
 import UpcomingEvents from './UpcomingEvents'
 
-const UserScreen = () => {
+const UserScreen = ({ user, joinedEvents }) => {
+    const { user_name, following_categories } = user
     return (
         <div className='User-Screen'>
-            <UserEvents />
+            <UserEvents 
+                userName={user_name}
+                joinedEvents={joinedEvents}
+                followingCategories={following_categories}
+            />
             <UpcomingEvents />
         </div>
     )
