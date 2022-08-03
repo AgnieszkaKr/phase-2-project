6bef0d2ee8d9b84f5c454e2ee2ff861ea9f08ab1
Agnes <agsawicka@gmail.com>
2022-08-03 09:53:45 -0400

    Agnes changes

--- src/components/AvaliableEvents.js
+++ src/components/AvaliableEvents.js
@@ -18,11 +18,18 @@ const AvaliableEvents = ({events}) => {
 
     return (
         <div>
-        <div className="filters"> 
-            <button className="filter-button" >search by date </button>
-            <button className="filter-button"> search by category </button>
-            <input className="filter-input" type="text" placeholder="search by keyword" value={searchKey} onChange={handleSearchKey}/>
-        </div>  
+        <div classNam='filters'> 
+            <button className='filter-button' >search by date </button>
+            <button className='filter-button'> search by category </button>
+            <input className='filter-input' type='tex=' placeholder='search by keyword' value={searchKey} onChange={handleSearchKey}/>
+        </div> 
+        <div className='Events-Group'>
+            <img src='https://cdn.dribbble.com/users/2704931/screenshots/7048399/media/e9125c8957513a73a246798b8b21350d.png?compress=1&resize=800x600&vertical=top' alt='interview'className='Group-Event-Image'/>
+            <img src='https://raw.githubusercontent.com/DXHeroes/knowledge-base-content/master/files/pair-programming.svg?sanitize=true' alt='pair-programming' className='Group-Event-Image'/>
+            <img src='https://t3.ftcdn.net/jpg/02/83/03/80/360_F_283038002_b0lUl4BSXEEICv0VIZX2Em6mAQe8vMkN.jpg' alt='hackathon' className='Group-Event-Image'/>
+
+
+        </div>
         <div className='AvaliableEvents'>
             {filter.map(event => <AvaliableEvent event={event}/> )}
 
